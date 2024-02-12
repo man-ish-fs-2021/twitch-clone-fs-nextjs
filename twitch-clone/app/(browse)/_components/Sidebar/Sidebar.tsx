a456d41a8349ab3f71af28b9f35f70a6e9390843
Manish Chanra <manishchandra58@gmail.com>
2024-02-12 19:31:14 +0530

    Container Ui added for sidebar

---
+++ twitch-clone/app/(browse)/_components/Sidebar/Sidebar.tsx
@@ -0,0 +1,15 @@
+import Toggle from "./Toggle";
+import Wrapper from "./wrapper";
+
+const Sidebar = () => {
+  // server component
+  // follow users
+
+  return (
+    <Wrapper>
+      <Toggle />
+    </Wrapper>
+  );
+};
+
+export default Sidebar;
--- twitch-clone/app/(browse)/layout.tsx
+++ twitch-clone/app/(browse)/layout.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "./_components/navbar/Navbar";
+import Sidebar from "./_components/Sidebar/Sidebar";
+import Container from "./_components/Container/Container";
 
 const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      <div className="flex h-full pt-20">{children}</div>
+      <div className="flex h-full pt-20">
+        <Sidebar />
+        <Container>{children}</Container>
+      </div>
     </>
   );
 };
